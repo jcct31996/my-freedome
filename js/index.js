4a03ae9e026926bf2c9566216df2470588fdc7e5
Justin Chua <justin.chua@f-secure.com>
2019-11-27 09:53:57 +0800

    .

--- js/index.js
+++ js/index.js
@@ -74,6 +74,7 @@ if (!Element.prototype.closest) {
       m._elements = {};
       m._elements.self = y;
       var s = m._elements.self.querySelectorAll("[data-" + f + "-hook-" + g + "]");
+      console.log("s: "+s);
       for (var v = 0; v < s.length; v++) {
         var w = s[v];
         if (w.closest("." + f + "-" + g) === m._elements.self) {
