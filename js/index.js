78a049a019ba3f9ed39a261915ba1b9b76b9a0ca
Justin Chua <justin.chua@f-secure.com>
2019-11-27 11:10:09 +0800

    .

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
-
 "use strict";
 var myBtn = function () {
   console.log('Your JS is loaded!');
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log('Your document is ready!');
-    var myBtn = document.querySelectorAll(".myBtn");
-    console.log("myBtn: " + myBtn);
-    console.log("myBtn[0]: " + myBtn[0]);
-    console.log("myBtn[1]: " + myBtn[1]);
-    var i;
-    for (i = 0; i < myBtn.length; i++) {
-      console.log("myBtn[i]: " + myBtn[i]);
-      myBtn[i].addEventListener("click", function () {
-        alert("clicked");
-      });
-    }
-  });
+  console.log('Your document is ready!');
+  var myBtn = document.querySelectorAll(".myBtn");
+  console.log("myBtn: " + myBtn);
+  console.log("myBtn[0]: " + myBtn[0]);
+  console.log("myBtn[1]: " + myBtn[1]);
+  var i;
+  for (i = 0; i < myBtn.length; i++) {
+    console.log("myBtn[i]: " + myBtn[i]);
+    myBtn[i].addEventListener("click", function () {
+      alert("clicked");
+    });
+  }
+
 }();
 
 /*
