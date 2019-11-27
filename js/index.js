9a3cd5b5f73aded659ed8c47200f0861e147688f
Justin Chua <justin.chua@f-secure.com>
2019-11-27 11:18:39 +0800

    .

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
-"use strict";
-var myBtn = function () {
-  console.log('Your JS is loaded!');
-  console.log('Your document is ready!');
-  var myBtn = document.querySelectorAll(".myBtn");
-  console.log("myBtn: " + myBtn);
-  console.log("myBtn[0]: " + myBtn[0]);
-  console.log("myBtn[1]: " + myBtn[1]);
-  var i;
-  for (i = 0; i < myBtn.length; i++) {
-    console.log("myBtn[i]: " + myBtn[i]);
-    myBtn[i].addEventListener("click", function () {
-      alert("clicked");
-    });
-  }
 
-}();
+"use strict";
+(function () {
+  console.log('Your JS is loaded!');
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log('Your document is ready!');
+    var myBtn = document.querySelectorAll(".myBtn");
+    console.log("myBtn: " + myBtn);
+    console.log("myBtn[0]: " + myBtn[0]);
+    console.log("myBtn[1]: " + myBtn[1]);
+    var i;
+    for (i = 0; i < myBtn.length; i++) {
+      console.log("myBtn[i]: " + myBtn[i]);
+      myBtn[i].addEventListener("click", function () {
+        alert("clicked");
+      });
+    }
+  });
+})();
 
 /*
 window.PPAemConnector = (function () {
